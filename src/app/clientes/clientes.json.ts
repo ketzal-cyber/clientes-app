23ed548f737755cc75efc4dd8c9d66a97d3d3d6b
ketzal-cyber <angelhernandez_1m368@hotmail.com>
2021-01-05 00:12:38 -0600

    modificacion de array clientes

--- src/app/clientes/clientes.json.ts
+++ src/app/clientes/clientes.json.ts
@@ -1,15 +1,15 @@
 import { Cliente } from './cliente';
 
 export const CLIENTES: Cliente[] = [
-  {id: 1, nombre: 'Agent01', apellido: 'appe01', createAt: '2021-01-03', email: 'agent@email.com'},
-  {id: 2, nombre: 'Mr. John', apellido: 'Doe', createAt: '2020-11-01', email: 'john@email.com'},
-  {id: 3, nombre: 'Linux', apellido: 'Torvalds', createAt: '2015-10-03', email: 'torvalds@email.com'},
-  {id: 4, nombre: 'Gausse', apellido: 'Jordan', createAt: '2015-10-03', email: 'jordan@email.com'},
-  {id: 5, nombre: 'Richard', apellido: 'Stateman', createAt: '2000-30-15', email: 'stateman@email.com'},
-  {id: 6, nombre: 'Eric', apellido: 'Gamma', createAt: '2019-10-25', email: 'gamma@email.com'},
-  {id: 7, nombre: 'Dr. James', apellido: 'Goslin', createAt: '2018-10-15', email: 'goslin@email.com'},
-  {id: 8, nombre: 'Bruce', apellido: 'Lee', createAt: '1995-09-29', email: 'lee@email.com'},
-  {id: 9, nombre: 'Tornado', apellido: 'viento', createAt: '2021-02-29', email: 'viento@email.com'},
-  {id: 10, nombre: 'Magna', apellido: 'verde', createAt: '2018-12-10', email: 'verde@email.com'},
-  {id: 11, nombre: 'Mandalorian', apellido: 'Mando', createAt: '2020-11-05', email: 'mondo@email.com'}
+  {id: 1, nombre: 'Din', apellido: 'Djarin', createAt: '2021-01-03', email: 'mando@email.com'},
+  {id: 2, nombre: 'Cara', apellido: 'Dune', createAt: '2020-11-01', email: 'dune@email.com'},
+  {id: 3, nombre: 'IG-11', apellido: 'Droide', createAt: '2015-10-03', email: 'droide_ig-11@email.com'},
+  {id: 4, nombre: 'Jango', apellido: 'Fett', createAt: '2015-10-03', email: 'jfett@email.com'},
+  {id: 5, nombre: 'Boba', apellido: 'Fett', createAt: '2000-30-15', email: 'bfett@email.com'},
+  {id: 6, nombre: 'Grogu', apellido: 'Baby Yoda', createAt: '2019-10-25', email: 'baby_yoda@email.com'},
+  {id: 7, nombre: 'Ashoka', apellido: 'Tano', createAt: '2018-10-15', email: 'tanon@email.com'},
+  {id: 8, nombre: 'Moff', apellido: 'Gideon', createAt: '1995-09-29', email: 'lee@email.com'},
+  {id: 9, nombre: 'Kuiil', apellido: 'IngKuiil', createAt: '2021-02-29', email: 'ingkuiil@email.com'},
+  {id: 10, nombre: 'Greef', apellido: 'Karga', createAt: '2018-12-10', email: 'karga@email.com'},
+  {id: 11, nombre: 'Bo', apellido: 'Katan', createAt: '2020-11-05', email: 'bokatan@email.com'}
 ];
