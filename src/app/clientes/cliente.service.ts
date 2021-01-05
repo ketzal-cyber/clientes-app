b1694caee071a80fad7934153eaace30edc531f6
ketzal-cyber <angelhernandez_1m368@hotmail.com>
2021-01-05 15:47:55 -0600

    mmodificar para obtener datos del backend apirest

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,14 +2,25 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 
 @Injectable()
 export class ClienteService {
 
-  constructor() { }
+private urlEndPoint: string = 'http://localhost:9999/api/clientes';
+//inyectar una vraible HttpClient
+//ademas la variable queda como vvariable de clase
+  constructor(private http: HttpClient ) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    //return of(CLIENTES);
+    // se hace un cast a un Observable en esta forma
+        //return this.http.get<Cliente[]>(this.urlEndPoint)
+    //otra forma de convertir el any JSon a un cleinte[]es
+    return this.http.get(this.urlEndPoint).pipe(
+      map( (response) => response as Cliente[] )
+    );
   }
 }
