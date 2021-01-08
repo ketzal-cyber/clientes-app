5b1590d06cabbe512728bfae5eb8ee7dcb108d91
ketzal-cyber <angelhernandez_1m368@hotmail.com>
2021-01-08 00:24:27 -0600

    implementar libreria sweetalert2

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -22,8 +23,11 @@ public titulo: string = "Crear Cliente";
     console.log("Clicket");
     console.log(this.cliente);
 
-    this.clienteService.create(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
+    this.clienteService.create(this.cliente)
+    .subscribe(cliente => {
+      this.router.navigate(['/clientes'])
+      swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} credo con exito`, 'success')
+    }
     )
 
   }
