e9581010679d976daebae6f7c2e567734fc48e6d
ketzal-cyber <angelhernandez_1m368@hotmail.com>
2021-01-08 02:21:09 -0600

    implementando el metodo delete

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -17,6 +18,43 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void {
+
+    const swalWithBootstrapButtons = swal.mixin({
+  customClass: {
+    confirmButton: 'btn btn-success',
+    cancelButton: 'btn btn-danger'
+  },
+  buttonsStyling: false
+})
+
+swalWithBootstrapButtons.fire({
+  title: '¿Estas seguro?',
+  text: `¿Estas seguro de eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonText: 'Sí, eliminar',
+  cancelButtonText: 'No, cancelar',
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) {
+      this.clienteService.delete(cliente.id).subscribe(
+        response => {
+          this.clientes = this.clientes.filter(cli => cli !== cliente)
+          swalWithBootstrapButtons.fire(
+            'Cliente eliminado',
+            `Cliente ${cliente.nombre} eliminado con exito`,
+            'success'
+          )
+        }
+      )
+
+
+  }
+})
+
   }
 
 }
