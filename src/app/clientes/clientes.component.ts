9324c2860bfa61e9d93959d92bc13f48bc12d6ea
ketzal-cyber <angelhernandez_1m368@hotmail.com>
2021-02-04 22:22:10 -0600

    agregar ruta paginador y observable

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -3,6 +3,7 @@ import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import swal from 'sweetalert2';
 import { tap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-clientes',
@@ -13,23 +14,31 @@ export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
 
-  constructor( private clienteService: ClienteService) { }
+  constructor( private clienteService: ClienteService,
+  private activatedRoute: ActivatedRoute) { }
 ;
 
   /*Importar tap se podria utiliar en el getClientes() .pipe(),tap()
   */
   ngOnInit(): void {
-    let page = 0;
-    this.clienteService.getClientes(page)
-    .pipe(
-      tap(response => {
-        console.log('ClienteComponent: tap3');
-        (response.content as Cliente[]).forEach(cliente => {
-          console.log(cliente.nombre);
-        });
-      })
-      // .subscribe( clientes => this.clientes = clientes );
-    ).subscribe( response => this.clientes = response.content as Cliente[] );
+    this.activatedRoute.paramMap.subscribe( params => {
+      let page:number = +params.get('page');
+
+      if(!page){
+        page = 0;
+      }
+      this.clienteService.getClientes(page)
+      .pipe(
+        tap(response => {
+          console.log('ClienteComponent: tap3');
+          (response.content as Cliente[]).forEach(cliente => {
+            console.log(cliente.nombre);
+          });
+        })
+        // .subscribe( clientes => this.clientes = clientes );
+      ).subscribe( response => this.clientes = response.content as Cliente[] );
+    }
+    );
   }
 
   delete(cliente: Cliente): void {
