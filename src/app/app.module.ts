ca5439a474c7c5ba851d212aceac4d51d2210520
ketzal-cyber <angelhernandez_1m368@hotmail.com>
2021-02-05 23:35:40 -0600

    implementacion de paginacion en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { FormComponent } from './clientes/form.component';
+import { PaginatorComponent } from './paginator/paginator.component';
+
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { FormComponent } from './clientes/form.component';
 import { FormsModule } from '@angular/forms';
+
 
 /*lineas para formato de fechas de manera global
 */
@@ -34,7 +37,8 @@ const routes: Routes = [
     FooterComponent,
     DirectivaComponent,
     ClientesComponent,
-    FormComponent
+    FormComponent,
+    PaginatorComponent
 
   ],
   imports: [
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -13,6 +13,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
+  paginador: any;
 
   constructor( private clienteService: ClienteService,
   private activatedRoute: ActivatedRoute) { }
@@ -36,7 +37,10 @@ export class ClientesComponent implements OnInit {
           });
         })
         // .subscribe( clientes => this.clientes = clientes );
-      ).subscribe( response => this.clientes = response.content as Cliente[] );
+      ).subscribe( response => {
+        this.clientes = response.content as Cliente[];
+        this.paginador = response;
+      } );
     }
     );
   }
